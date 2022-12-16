1b04b9e1db1a5b77ccad2cb13992b24ee262fc09
unknown <sawant.yuga13@gmail.com>
2022-12-16 20:02:35 +0530

    Added all files

---
+++ config1.py
@@ -0,0 +1,10 @@
+
+import os
+
+CSV_FILE_PATH = "medical_insurance.csv"
+
+PORT_NUMBER = 5005
+
+MODEL_FILE_PATH = os.path.join("models", "linear_reg_model.pkl")
+
+JSON_FILE_PATH = os.path.join("models", "project_data.json")
